5abc93860e676451c96c80b8ed82067d5e730dbc
JuanEraz0 <juanerazo.fi@gmail.com>
2024-07-23 23:10:50 -0600

    curso de plataformas Web IoT

---
+++ clase1_fdjs.js
@@ -0,0 +1,15 @@
+//Tipos de variables
+
+//VAR 
+
+var numero = 10;
+console.log(numero);
+
+function foo(){
+    numero = 20;
+
+}
+
+foo();
+console.log(numero);
+
---
+++ prueba.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//using namespace std;
+
+int main(){
+
+    std::cout<<"Hola mundo en C++"<<std::endl;
+
+    return 0;
+
+}
+
