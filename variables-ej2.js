472f848d3bc9c7b035d0786855e220e811358c5c
JuanEraz0 <juanerazo.fi@gmail.com>
2024-07-23 23:32:30 -0600

    tipos de variables en js

---
+++ variables-ej2.js
@@ -0,0 +1,34 @@
+
+//ejemplo con var
+
+var i =0;
+
+if (true){
+    var i = 4;
+    console.log(i);
+}
+
+console.log(i);
+
+//ejemplo con let
+
+let j =0;
+
+if (true){
+    let j = 4;
+    console.log(j);
+}
+
+console.log(j);
+
+
+// ejemplo con const
+
+const c = 34;
+if (true){
+    console.log(c);
+}
+
+
+
+
